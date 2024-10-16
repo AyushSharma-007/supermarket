23642a92205ae4f1bbd1fc42f718360ed88563d5
AYUSH SHARMA <120780215+AyushSharma-007@users.noreply.github.com>
2024-10-17 00:19:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+n = pd.read_csv(r"C:\Users\ACER PC\Dropbox\My PC (LAPTOP-83BUHU5P)\Downloads\Sample - Superstore.csv\Sample - Superstore.csv",encoding='ISO-8859-1')
+n
+
+
